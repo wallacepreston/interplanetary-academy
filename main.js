c4e8aee41f901bfa749bb65db6ec91664fd4830a
Preston Wallace <wallace.preston@gmail.com>
2018-12-11 23:36:03 -0800

    chore: Add webpack as normal dependency

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 
 const {db} = require('./server/db')
 const app = require('./server')
-const PORT = 1337
+const port = process.env.PORT || 1337
 
 db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
   .then(() => {
     console.log('db synced')
-    app.listen(process.env.PORT || PORT, () => console.log(`studiously serving silly sounds on port ${PORT}`))
+    app.listen(port, () => console.log(`studiously serving silly sounds on port ${port}`))
   })
