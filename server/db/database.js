e0bd9c374a5286469a56e582e978278feac9aad3
Preston Wallace <wallace.preston@gmail.com>
2018-12-12 00:01:22 -0800

    Chore: Changed PostgreSQL URI

--- server/db/database.js
+++ server/db/database.js
@@ -12,7 +12,12 @@ console.log(chalk.yellow('Opening database connection'))
 
 // create the database instance that can be used in other database files
 // const db = new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
-const db = new Sequelize(`postgres://ec2-54-163-245-64.compute-1.amazonaws.com:5432/d6bk1bgmp7tac0`, {
+// const db = new Sequelize(`postgres://ec2-54-163-245-64.compute-1.amazonaws.com:5432/d6bk1bgmp7tac0`, {
+// const db = new Sequelize('d6bk1bgmp7tac0', 'tftdtwrndheqqu', 'b44bcc8d6f18a44c6f33a547949ac174d8ece96797576d59d682c9e3979e9282', {
+//   host: 'ec2-54-163-245-64.compute-1.amazonaws.com',
+//   port: 5432,
+const db = new Sequelize(`postgres://tftdtwrndheqqu:b44bcc8d6f18a44c6f33a547949ac174d8ece96797576d59d682c9e3979e9282@ec2-54-163-245-64.compute-1.amazonaws.com:5432/d6bk1bgmp7tac0
+`, {
   logging: false // so we don't see all the SQL queries getting made
 })
 
