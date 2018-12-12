a6598dda5a5f437a9938aaf831a83b8fde5db17c
Preston Wallace <wallace.preston@gmail.com>
2018-12-11 23:40:40 -0800

    chore: Edit DB URL for deployment. Add  to beginning of URL

--- server/db/database.js
+++ server/db/database.js
@@ -12,7 +12,7 @@ console.log(chalk.yellow('Opening database connection'))
 
 // create the database instance that can be used in other database files
 // const db = new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
-const db = new Sequelize(`ec2-54-163-245-64.compute-1.amazonaws.com:5432/d6bk1bgmp7tac0`, {
+const db = new Sequelize(`postgres://ec2-54-163-245-64.compute-1.amazonaws.com:5432/d6bk1bgmp7tac0`, {
   logging: false // so we don't see all the SQL queries getting made
 })
 
