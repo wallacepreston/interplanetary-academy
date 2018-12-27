2311349ca943125b0da24b17764efed95e8f6a4f
Preston Wallace <wallace.preston@gmail.com>
2018-12-26 23:33:35 -0800

    chore: Edited webpack.config.js in attempt to fix bug in module build failing in Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     path: __dirname,
-    filename: './public/bundle.js'
+    filename: './public/[name].js'
   },
   mode: 'development',
   context: __dirname,
