f8325f33b6610e19278293d1a69bad5f180122dd
Preston Wallace <wallace.preston@gmail.com>
2019-02-03 16:11:27 -0800

    Revert "initial commit"
    This reverts commit f9a2cddbe62135b674afc319c3c34ac7a665270c.

--- server/db/database.js
+++ server/db/database.js
@@ -12,6 +12,7 @@ console.log(chalk.yellow('Opening database connection'))
 // const db = new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
 const db = new Sequelize(
   process.env.DATABASE_URL || // FOR DEPLOYED APP
+  // `postgres://tftdtwrndheqqu:b44bcc8d6f18a44c6f33a547949ac174d8ece96797576d59d682c9e3979e9282@ec2-54-163-245-64.compute-1.amazonaws.com:5432/d6bk1bgmp7tac0`  // FOR DEPLOYED APP
   `postgres://localhost:5432/${pkg.name}` // FOR DEVELOPMENT
   , {
     logging: false // so we don't see all the SQL queries getting made
